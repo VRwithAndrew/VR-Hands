9d99ef6cdd13774e811c26361f2cc415a1333b82
Andrew <drew.l.connell@gmail.com>
2019-04-27 18:31:47 -0400

    Initial Project

---
+++ Assets/_VRHands/Scripts/HandAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimator : MonoBehaviour
+{
+    private void Awake()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}
